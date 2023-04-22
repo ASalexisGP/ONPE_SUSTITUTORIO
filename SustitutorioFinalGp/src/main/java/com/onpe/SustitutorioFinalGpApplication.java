8ead0ca6d94d70fe8500ba3703f9fbb15edfc0a2
alexi <you@example.comexitsalirhomea git config --global user.email you@example.com>
2023-04-21 19:51:11 -0500

    COMENZANDO 1.0

---
+++ SustitutorioFinalGp/src/main/java/com/onpe/SustitutorioFinalGpApplication.java
@@ -0,0 +1,13 @@
+package com.onpe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SustitutorioFinalGpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SustitutorioFinalGpApplication.class, args);
+	}
+
+}
